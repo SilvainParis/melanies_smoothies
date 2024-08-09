410b9605de1c2eb22d2fbcf77f5863ec979eaafd
SilvainParis <sdelencre@synergy.fr>
2024-08-09 17:02:10 +0200

    Update streamlit_app.py
    'Search on' alternative

--- streamlit_app.py
+++ streamlit_app.py
@@ -17,8 +17,9 @@ st.write("The name of your smoothie will be :", name_on_order)
 
 cnx=st.connection("snowflake")
 session = cnx.session()
-my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'))
-#st.dataframe(data=my_dataframe, use_container_width=True)
+my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'),col('SEARCH_ON'))
+st.dataframe(data=my_dataframe, use_container_width=True)
+st.stop()
 
 ingredient_list = st.multiselect(
         'Choose up to 5 ingredients:'
@@ -32,7 +33,7 @@ if ingredient_list:
 
     for fruit_chosen in ingredient_list:
         ingredients_string+= fruit_chosen + ' '
-        st.subheader(fruit_chosen + 'Nutrition information')
+        st.subheader(fruit_chosen + ' nutrition information')
         fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+fruit_chosen)
         fv_df=st.dataframe(data=fruityvice_response.json(), use_container_width=True)
 
