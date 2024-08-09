115de7e6407413be17772b35e5a703fc6e4fc9d9
SilvainParis <sdelencre@synergy.fr>
2024-08-09 17:38:21 +0200

    Update streamlit_app.py
    session.sql('use warehouse compute_wh')

--- streamlit_app.py
+++ streamlit_app.py
@@ -17,7 +17,7 @@ st.write("The name of your smoothie will be :", name_on_order)
 
 cnx=st.connection("snowflake")
 session = cnx.session()
-session.sql('use warehouse compute_wh')
+#session.sql('use warehouse compute_wh')
 my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'),col('SEARCH_ON'))
 
 pd_df=my_dataframe.to_pandas()
@@ -53,5 +53,6 @@ if ingredient_list:
     time_to_insert = st.button('Submit order')
 
     if time_to_insert:
+        session.sql('use warehouse compute_wh')
         session.sql(my_insert_stmt).collect()
         st.success('Your Smoothie is ordered!',icon="✅")
